60f54fc25f8d33cac79171b76ac9d03551d82b50
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2020-10-20 15:26:26 +0800

    [Database_Structure][adrian]refactor: edited several columns in students table

--- database/migrations/2020_10_19_075836_create_students_table.php
+++ database/migrations/2020_10_19_075836_create_students_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 class CreateStudentsTable extends Migration
 {
     /**
@@ -15,12 +16,12 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('last_name');
+            $table->string('last_name')->nullable();
             $table->string('first_name');
             $table->string('middle_name')->nullable();
             $table->string('middle_initial')->nullable();
-            $table->string('nick_name')->nullable();
-            $table->string('gender');
+            $table->string('nickname')->nullable();
+            $table->string('gender')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });

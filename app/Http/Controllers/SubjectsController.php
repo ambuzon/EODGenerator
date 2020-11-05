b1d47ca486a9a0fef3c38800edb5394a6eb4350b
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2020-11-05 16:16:48 +0800

    [CRUD][adrian]refactor: United all subjects controller

---
+++ app/Http/Controllers/SubjectsController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubjectsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function english()
+    {
+        function getComments($criteria){
+            $subject_id = Subject::where('Subject', 'English')->pluck('Id');
+            $comments = Comment::where('SubjectId', $subject_id)
+                        ->where('Criteria', $criteria)
+                        ->pluck('Comment');
+            return $comments;
+        }
+        $comments = array(
+            'comments1' => getComments('Q1'),
+            'comments2' => getComments('Q2'),
+            'comments3' => getComments('Q3'),
+            'comments4' => getComments('Q4'),
+        );
+        return view('subjects.english')->with($comments);
+    }
+
+    public function global()
+    {
+        function getComments($criteria){
+            $subject_id = Subject::where('Subject', 'Global')->pluck('Id');
+            $comments = Comment::where('SubjectId', $subject_id)
+                        ->where('Criteria', $criteria)
+                        ->pluck('Comment');
+            return $comments;
+        }
+        $comments = array(
+            'comments1' => getComments('Q1'),
+            'comments2' => getComments('Q2'),
+            'comments3' => getComments('Q3'),
+            'comments4' => getComments('Q4'),
+            'comments4a' => getComments('Q4a'),
+        );
+        return view('subjects.global')->with($comments);
+    }
+
+    public function math()
+    {
+        function getComments($criteria){
+            $subject_id = Subject::where('Subject', 'Math')->pluck('Id');
+            $comments = Comment::where('SubjectId', $subject_id)
+                        ->where('Criteria', $criteria)
+                        ->pluck('Comment');
+            return $comments;
+        }
+        $comments = array(
+            'comments1' => getComments('Q1'),
+            'comments2a' => getComments('Q2a'),
+            'comments2c' => getComments('Q2c'),
+            'comments3a' => getComments('Q3a'),
+            'comments3b' => getComments('Q3b'),
+            'comments3c' => getComments('Q3c'),
+            'comments4a' => getComments('Q4a'),
+            'comments4b' => getComments('Q4b'),
+            'comments4c1' => getComments('Q4c1'),
+            'comments4c2' => getComments('Q4c2'),
+            'comments4c3' => getComments('Q4c3'),
+            'comments4c4' => getComments('Q4c4'),
+        );
+        
+            return view('subjects.math')->with($comments);
+    }
+
+    public function science()
+    {
+        function getComments($criteria){
+            $subject_id = Subject::where('Subject', 'Science')->pluck('Id');
+            $comments = Comment::where('SubjectId', $subject_id)
+                        ->where('Criteria', $criteria)
+                        ->pluck('Comment');
+            return $comments;
+        }
+        $comments = array(
+            'comments1' => getComments('Q1'),
+            'comments2' => getComments('Q2'),
+            'comments3' => getComments('Q3'),
+            'comments4' => getComments('Q4'),
+        );
+        return view('subjects.science')->with($comments);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\EnglishController;
-use App\Http\Controllers\MathController;
-use App\Http\Controllers\ScienceController;
-use App\Http\Controllers\GlobalController;
+use App\Http\Controllers\SubjectsController;
 /*
 /*
 /*
@@ -29,14 +26,4 @@ use App\Http\Controllers\GlobalController;
 Route::get('/', [HomeController::class, 'index']);
 Route::post('/submit', [HomeController::class, 'submit']);
 
-Route::get('/english', [EnglishController::class, 'index']);
-Route::post('/english/submit', [EnglishController::class, 'submit']);
-
-Route::get('/math', [MathController::class, 'index']);
-Route::post('/math/submit', [MathController::class, 'submit']);
-
-Route::get('/science', [ScienceController::class, 'index']);
-Route::post('/science/submit', [EnglishController::class, 'submit']);
-
-Route::get('/global', [GlobalController::class, 'index']);
-Route::post('/global/submit', [GlobalController::class, 'submit']);
+Route::resource('subjects', SubjectsController::class);
